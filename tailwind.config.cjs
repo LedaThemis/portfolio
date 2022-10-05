1122177478596b12fcb59c6f699b09694aa61762
Leda <ledathemis@protonmail.com>
2022-10-05 10:26:14 +0000

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      primary: '#ede7e1',
+      secondary: '#ffffff',
+    },
+  },
+};
