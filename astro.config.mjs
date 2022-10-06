2faac5f0ce0851d543a29e7d5523cfa8be5a41e3
Leda <ledathemis@protonmail.com>
2022-10-06 06:31:12 +0000

    feat: configure SSR with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
+      external: ['svgo']
+    }
   },
+  adapter: vercel()
 });
