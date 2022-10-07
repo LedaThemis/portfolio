fba6ab2277edab755880abbd8c2fb80a0c73a70d
Leda <ledathemis@protonmail.com>
2022-10-07 15:49:15 +0000

    feat: use config to store values

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default {
+  personal: {
+    github: 'https://github.com/LedaThemis',
+    email: 'contact@leda.dev',
+  },
+  portfolio: {
+    github: 'https://github.com/LedaThemis/portfolio',
+  },
+};
