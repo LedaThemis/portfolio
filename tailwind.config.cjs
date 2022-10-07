98392f15b55bf89f7a41996c296fe8fcf147a3f4
Leda <ledathemis@protonmail.com>
2022-10-07 16:03:21 +0000

    feat: add body font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         title: ['"Nunito Sans"', ...defaultTheme.fontFamily.sans],
+        body: ['Roboto', ...defaultTheme.fontFamily.sans],
       },
     },
   },
