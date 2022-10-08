67e037eac863114c3fc8e5482e11bbcfe4175654
Leda <ledathemis@protonmail.com>
2022-10-08 17:49:45 +0000

    fix: remove astro-icon to solve svgo dependency
    This concerns the vercel deployment showing:
    
    Cannot find package 'svgo' imported from /var/task/entry.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://leda.dev',
-  integrations: [tailwind(), sitemap({
-    customPages: ['https://leda.dev']
-  })],
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  },
-  adapter: vercel()
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ['https://leda.dev'],
+    }),
+  ],
+  adapter: vercel(),
 });
