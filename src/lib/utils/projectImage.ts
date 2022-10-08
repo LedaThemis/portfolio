1bedaad87328e815882dd354f04d167cd002ea39
Leda <ledathemis@protonmail.com>
2022-10-08 16:49:19 +0000

    feat: use webp, add format

--- src/lib/utils/projectImage.ts
+++ src/lib/utils/projectImage.ts
@@ -4,7 +4,7 @@ import type { IProject } from '../interfaces/Project';
 const imageAPIURL = (hash: string, ext: string, width: string) => {
   const { baseURL, instanceName } = config.providers.cloudinary;
 
-  const url = `${baseURL}/${instanceName}/image/upload/w_${width},c_scale/${hash}${ext}`;
+  const url = `${baseURL}/${instanceName}/image/upload/w_${width},c_scale/${hash}${'.webp'}`;
 
   return url;
 };
@@ -14,6 +14,7 @@ export const constructImageSrcSet = (image: IProject['data']['attributes']['cove
     `${imageAPIURL(image.data.attributes.hash, image.data.attributes.ext, '1440')} 1440w`,
     `${imageAPIURL(image.data.attributes.hash, image.data.attributes.ext, '1080')} 1080w`,
     `${imageAPIURL(image.data.attributes.hash, image.data.attributes.ext, '720')} 720w`,
+    `${imageAPIURL(image.data.attributes.hash, image.data.attributes.ext, '500')} 500w`,
     `${imageAPIURL(image.data.attributes.hash, image.data.attributes.ext, '360')} 360w`,
   ].join(', ');
 };
