bc8657b6b1eba231ae3708c56232bcfd78869f5e
Leda <ledathemis@protonmail.com>
2022-10-31 16:20:26 +0000

    feat: switch from serverless to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  site: "https://leda.dev",
-  integrations: [
-    tailwind(),
-    sitemap({
-      customPages: ["https://www.leda.dev"],
-    }),
-    mdx(),
-  ],
-  adapter: vercel(),
+  site: "https://www.leda.dev",
+  integrations: [tailwind(), sitemap(), mdx()],
 });
