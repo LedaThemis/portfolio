254811cbb0365906b4deb14404b249257c63ae54
Leda <ledathemis@protonmail.com>
2022-10-31 16:36:43 +0000

    chore: remove unused env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly API_ENDPOINT: string;
-  readonly API_TOKEN: string;
   readonly FORM_API_ENDPOINT: string;
   readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
 }
