d8f04c2d054755ced350730121562ac999820d48
Leda <ledathemis@protonmail.com>
2022-11-05 06:50:08 +0000

    feat(ci): add auto-update screenshot

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,5 @@
 interface ImportMetaEnv {
   readonly FORM_API_ENDPOINT: string;
   readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+  readonly GH_TOKEN: string;
 }
---
+++ updatePreviewScreenshot.js
@@ -0,0 +1,45 @@
+import captureWebsite from "capture-website";
+import dotenv from "dotenv";
+import { Octokit } from "@octokit/core";
+
+dotenv.config();
+
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
+
+const main = async () => {
+  const base64image = await captureWebsite.base64("https://www.leda.dev", {
+    delay: 2,
+  });
+
+  const previewImage = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner: "LedaThemis",
+      repo: "portfolio",
+      path: "preview.png",
+    }
+  );
+
+  if (previewImage.data.content === base64image) {
+    console.log("Nothing to update...");
+  } else {
+    await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+      owner: "LedaThemis",
+      repo: "portfolio",
+      path: "preview.png",
+      message: "chore: update preview screenshot",
+      committer: {
+        name: "Website Preview",
+        email: "bot@leda.dev",
+      },
+      content: base64image,
+      sha: previewImage.data.sha,
+    });
+  }
+};
+
+(async () => {
+  main();
+})();
