aa017f6d234ca22c63f7dc2f76dc1e820a27bbff
Leda Themis <me@leda.dev>
2026-02-11 21:27:46 +0300

    feat: update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.leda.dev",
+  site: "https://www.alqaba.com",
   integrations: [tailwind(), sitemap(), mdx()],
 });
