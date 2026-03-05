39b60b9fbb09ee99760aa3ec5b634131a2c54a90
Leda Themis <me@leda.dev>
2026-03-05 20:13:35 +0300

    feat: make events a content collection

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+import { defineCollection } from "astro:content";
+
+import { glob, file } from "astro/loaders";
+
+import { z } from "astro/zod";
+
+const events = defineCollection({
+  loader: file("src/data/events.json"),
+  schema: z.object({
+    title: z.string(),
+    role: z.string(),
+    reference: z.string().optional(),
+    location: z.object({
+      venue: z.string().optional(),
+      city: z.string(),
+      country: z.string(),
+    }),
+    start_date: z.coerce.date(),
+    end_date: z.coerce.date(),
+    mention: z.object({
+        lang: z.string(),
+        text: z.string(),
+    }).optional(),
+  }),
+});
+
+export const collections = { events };
